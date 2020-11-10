aaf81d9ee5cbccc54262016f2c41d4e2e20d34c2
andrew nouanthanuvanh <andrewnouanthanuvanh@MacBook-Pro-3.local>
2020-11-09 23:28:13 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+/* HARD CHALLEGE */
+
+const massTom = 8;
+const heightTom = 9;
+const massJerry = 45;
+const heightJerry = 10;
+
+const BMITom = massTom / heightTom ** 2;
+const BMIJerry = massJerry / (heightJerry * heightJerry);
+const tomHigherBMI = BMITom > BMIJerry;
+console.log(BMITom, BMIJerry);
+
+
+
+/* MEDIUM CHALLENGE */
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+
+
+
+
+/* EASY CHALLENGE */ 
+
+var students = ["Tien Do 22", "Michael Jordan 23", "Joseph Gribble 17"];
+
+console.log(students [1]= "Hello, my name is Michael Jordan and I'm Number 23.");
+
+
+
+
+
+/* VERY EASY CHALLENGE */ 
+
+function min( firstNum, secondNum) {
+    if ( firstNum < secondNum) 
+    return firstNum;
+    else
+    return secondNum;
+}
+
+console.log(min(0,10));
+console.log(min(0, -10));
+
+
+
+
